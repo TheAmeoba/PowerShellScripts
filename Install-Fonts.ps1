3a063f4f50d0d3534f2d2e0545080e7fa4d95457
TheAmeoba <theameoba@gmail.com>
2018-02-08 14:56:34 +1100

    Install otf Fonts
    Install-Fonts - Support for otf files added

--- Install-Fonts.ps1
+++ Install-Fonts.ps1
@@ -9,6 +9,11 @@
 
     .NOTES
         Author: Simon Baker
+
+        Modified: 2017-09-20
+        -Added support for otf files
+        -Added comments to code
+
         Created: 2016-03-16
 #>
 
@@ -17,7 +22,17 @@ Param(
     [string]$Path = "."
 )
 
+# Create Shell Application
 $sa =  new-object -comobject shell.application
+
+# Define font namespace
 $Fonts =  $sa.NameSpace(0x14)
 
-Get-ChildItem  "$Path\*.ttf" | %{$fonts.CopyHere($_.FullName)}
+# Get list of fonts to install
+$fontlist = Get-ChildItem  "$Path\*.ttf"
+$fontlist += Get-ChildItem  "$Path\*.otf"
+Write-Verbose $fontlist
+write-debug "Font list gathered, ready for install"
+
+# Install fonts
+$fontlist | %{$fonts.CopyHere($_.FullName)}
